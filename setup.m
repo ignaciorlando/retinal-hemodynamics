d28c37111ab26653e6670c2a9ec231b2ea9632e5
ignaciorlando <ignaciorlando@gmail.com>
2017-10-26 17:13:48 -0300

    Library for skeletonizations

--- setup.m
+++ setup.m
@@ -17,6 +17,26 @@ if exist('configuration', 'file')==0
     copyfile('default-configuration', 'configuration');
 end
 
+% Install external libraries
+if exist('external', 'file')==0
+    mkdir('external');
+end
+
+% Skeletonization library
+skeletonization_library = fullfile('external', 'skeletonization');
+if exist(skeletonization_library, 'file') == 0
+    % Download the library
+    websave('Skeleton.zip', 'http://www.cs.smith.edu/~nhowe/research/code/Skeleton.zip');
+    % unzip the code
+    mkdir(skeletonization_library);
+    unzip('Skeleton.zip', fullfile('external', 'skeletonization'));
+    delete('Skeleton.zip')
+end
+% compile the library
+disp('Compiling anaskel.cpp...');
+mex 'external/skeletonization/anaskel.cpp'
+
+
 % add main folders to path
 addpath(genpath(fullfile(my_root_position, 'data-organization'))) ;
 addpath(genpath(fullfile(my_root_position, 'fundus-util'))) ;
