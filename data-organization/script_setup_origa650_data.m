66d45e929b504a67951fe4945717d194945fac9a
Jose Orlando <jose.orlando@meduniwien.ac.at>
2018-02-14 10:10:17 +0100

    Data preparation routines for Origa now prepare all the data needed for the CNN experiments

--- data-organization/script_setup_origa650_data.m
+++ data-organization/script_setup_origa650_data.m
@@ -26,19 +26,31 @@ end
 origa_input_image_folder = fullfile(origa_folder, 'images');
 origa_input_optic_disc_folder = fullfile(origa_folder, 'manual marking');
 origa_output_image_folder = fullfile(origa_output_folder, 'images');
+origa_output_image_onh_folder = fullfile(origa_output_folder, 'images-onh');
+origa_output_image_fov_folder = fullfile(origa_output_folder, 'images-fov');
+origa_output_image_fov_wo_onh_folder = fullfile(origa_output_folder, 'images-fov-wo-onh');
+
+% precomputed data folder
+% (don't change this parameter!)
+precomputed_data_folder = fullfile(pwd, 'precomputed-data', 'origa650', 'masks');
 
 %% crop the images around the FOV and resize
 
 % get image filenames
 image_filenames = dir(fullfile(origa_input_image_folder, '*.jpg'));
 image_filenames = { image_filenames.name };
-
+% get fov masks filenames
+fov_masks_filenames = dir(fullfile(precomputed_data_folder, '*.png'));
+fov_masks_filenames = { fov_masks_filenames.name };
 % get manual markings of the optic disc
 manual_markings_filenames = dir(fullfile(origa_input_optic_disc_folder, '*.mat'));
 manual_markings_filenames = { manual_markings_filenames.name };
 
 % create the output folder
 mkdir(origa_output_image_folder);
+mkdir(origa_output_image_onh_folder);
+mkdir(origa_output_image_fov_folder);
+mkdir(origa_output_image_fov_wo_onh_folder);
 
 % iterate for each image
 fprintf('Cropping and resizing images...\n');
@@ -49,23 +61,49 @@ for i = 1 : length(image_filenames)
     % prepare input/output image filenames
     current_input_image_name = image_filenames{i};
     current_output_image_name = strcat(current_input_image_name(1:end-3), 'png');
+
+    % prepare the image as it is
     
     % open the image
-    image = imread(fullfile(origa_input_image_folder, current_input_image_name));    
+    image = imread(fullfile(origa_input_image_folder, current_input_image_name));   
+    % save the image as it is
+    imwrite(image, fullfile(origa_output_image_folder, current_output_image_name));
+    
+    % prepare the image cropped around the fov mask
+    
+    % load the FOV mask from the precomputed file folder
+    fov_mask = imread(fullfile(precomputed_data_folder, fov_masks_filenames{i})) > 0;
+    fov_mask = fov_mask(:,:,1);
+    % crop the image
+    cropped_image_fov = crop_fov(image, fov_mask);
+    % resize the image
+    resized_image = imresize(cropped_image_fov, [224, NaN]);
+    % save the image as it is
+    imwrite(resized_image, fullfile(origa_output_image_fov_folder, current_output_image_name));
+    
+    % prepare the image cropped in the ONH
+    
     % load the marking
     load(fullfile(origa_input_optic_disc_folder, manual_markings_filenames{i}))
-    
     % dilate the mask to capture part of the tissue around the optic disc
     dilated_mask = imdilate(mask > 0, strel('disk', round(size(image, 1) * 0.05), 8));
-    
     % crop the image
     [ cropped_image, cropped_dilated_mask ] = crop_fov(image, dilated_mask);
-    
     % resize the image
     resized_image = imresize(cropped_image, [224, NaN]);
+    % save the image cropped around the ONH and resized for the CNN
+    imwrite(resized_image, fullfile(origa_output_image_onh_folder, current_output_image_name));
     
-    % save both the images and the fov mask
-    imwrite(resized_image, fullfile(origa_output_image_folder, current_output_image_name));
+    % prepare the image cropped in the FOV and without the ONH
+    
+    % crop the ONH mask
+    mask = crop_fov(mask, fov_mask);
+    % remove the onh
+    image_without_onh = uint8(double(cropped_image_fov) .* double(imcomplement(mask > 0)));
+    % resize the image
+    resized_image = imresize(image_without_onh, [224, NaN]);
+    % save the image
+    imwrite(resized_image, fullfile(origa_output_image_fov_wo_onh_folder, current_output_image_name));
     
 end
 
