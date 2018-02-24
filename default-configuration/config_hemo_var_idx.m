589a34dfa19e74ce77db0db8bb10b3c253985a0d
Unknown <carlos.alberto.bulant@gmail.com>
2018-02-24 06:51:04 -0300

    Missing file default configuration file.

---
+++ default-configuration/config_hemo_var_idx.m
@@ -0,0 +1,15 @@
+
+% CONFIG_HEMO_VAR_IDX
+% -------------------------------------------------------------------------
+% This script sets the indexes of the hemodynamic variables for the arrays 
+% containing the solutions.
+% -------------------------------------------------------------------------
+
+idx_r    = 1; % Radius
+idx_q    = 2; % Flow
+idx_p    = 3; % Pressure
+idx_v    = 4; % Velosity
+%idx_res  = 5; % Resistance
+%idx_re   = 6; % Reynolds
+%idx_wss  = 7; % Wall Shear Stress
+idx_mask = 5; % Mask
